2769c100d70ffed613c584c53f403436fb391a8c
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2025-02-18 23:09:10 -0300

    feat: validar formato de arquivo, aceitando apenas PDFs

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from services.pdf_extractor import extract_text_from_pdf
 from services.file_saver import save_text_to_json
 from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
-@router.post("/upload/pdf", summary="Upload de PDF e extração de texto")
+def validate_pdf(file: UploadFile):
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Apenas arquivos PDF são permitidos.")
+
+@router.post("/pdf", summary="Upload de PDF e extração de texto")
 async def upload_pdf(file: UploadFile = File(...)):
+    validate_pdf(file)
     text = extract_text_from_pdf(file)
     filepath = save_text_to_json(file.filename, text)
     return JSONResponse(content={"filename": file.filename, "filepath": filepath})
