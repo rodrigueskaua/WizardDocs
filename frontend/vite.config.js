426dfe024cfb99a3144749c894ccbdd1534f2cbd
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2025-04-03 20:54:23 -0300

    feat(frontend): adicionar configs do servidor Vite para ambiente de desenvolvimento com reload e cliente Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +11,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 5173,
     },
   },
 })
