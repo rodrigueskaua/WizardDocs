4b6200232ef2a4b27ff2d39ca41b2a9ca0ae43c1
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2025-04-09 02:11:36 -0300

    feat(backend): atualizar ai_service para usar o cliente OpenAI a partir de core.config para gerenciamento da chave da API

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -1,7 +1,7 @@
-import openai
-import os
+from core.config import settings
+from openai import OpenAI
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=settings.openai_api_key)
 
 def ask_openai(question: str, context: str) -> str:
   prompt = (
@@ -11,24 +11,22 @@ def ask_openai(question: str, context: str) -> str:
     "Resposta:"
   )
 
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages = [
-      {
-        "role": "system",
-        "content": (
-          "Você é um assistente especializado em compreender e responder perguntas com base em textos de documentos. "
-          "Seja claro, direto e objetivo nas respostas, evitando repetições e floreios desnecessários. "
-          "Caso a informação solicitada não esteja presente no contexto fornecido, diga que não foi possível encontrar a resposta."
-        )
-      },
-      {
-        "role": "user",
-        "content": prompt
-      }
-    ],
-    temperature=0.7,
-    max_tokens=500,
-  )
+  response = client.chat.completions.create(model="gpt-3.5-turbo",
+  messages = [
+    {
+      "role": "system",
+      "content": (
+        "Você é um assistente especializado em compreender e responder perguntas com base em textos de documentos. "
+        "Seja claro, direto e objetivo nas respostas, evitando repetições e floreios desnecessários. "
+        "Caso a informação solicitada não esteja presente no contexto fornecido, diga que não foi possível encontrar a resposta."
+      )
+    },
+    {
+      "role": "user",
+      "content": prompt
+    }
+  ],
+  temperature=0.7,
+  max_tokens=1000)
 
   return response.choices[0].message.content.strip()
