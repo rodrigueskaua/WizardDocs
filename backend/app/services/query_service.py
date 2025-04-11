a0ec2c5946c9220163f42c83c7234c41642a2e89
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2025-04-11 20:58:53 -0300

    refactor(backend): melhorar leitura da função query_knowledge_base e unifica lógica de limpeza de chunks

--- backend/app/services/query_service.py
+++ backend/app/services/query_service.py
@@ -2,18 +2,26 @@ from database.chromadb import retrieve_relevant_chunks
 from services.embedding_service import generate_embeddings_batch
 from services.ai_service import ask_openai
 
+def _clean_chunks(relevant_chunks):
+  return [
+    {
+      "text": ' '.join(chunk.replace("\n", " ").split()),
+      "distance": round(distance, 4)
+    }
+    for chunk, distance in relevant_chunks
+  ]
+
+def _get_context_from_chunks(clean_chunks):
+  return " ".join(chunk["text"] for chunk in clean_chunks)
+
 def query_knowledge_base(question: str, provider: str = "local", top_k: int = 5):
   query_embedding = generate_embeddings_batch(question)
+  relevant_chunks = retrieve_relevant_chunks("pdf_data", query_embedding, top_k=top_k)
+  
+  clean_chunks = _clean_chunks(relevant_chunks)
+  context = _get_context_from_chunks(clean_chunks)
 
   if provider == "local":
-    relevant_chunks = retrieve_relevant_chunks("pdf_data", query_embedding, top_k=top_k)
-    
-    clean_chunks = [
-      {"text": ' '.join(chunk.replace("\n", " ").split()), "distance": round(distance, 4)}
-      for chunk, distance in relevant_chunks
-    ]
-    context = " ".join(chunk["text"] for chunk in clean_chunks)
-    
     return {
       "question": question,
       "answer": context,
@@ -23,22 +31,14 @@ def query_knowledge_base(question: str, provider: str = "local", top_k: int = 5)
     }
 
   elif provider == "openai":
-    relevant_chunks = retrieve_relevant_chunks("pdf_data", query_embedding, top_k=top_k)
-    
-    clean_chunks = [
-      {"text": ' '.join(chunk.replace("\n", " ").split()), "distance": round(distance, 4)}
-      for chunk, distance in relevant_chunks
-    ]
-    context = " ".join(chunk["text"] for chunk in clean_chunks)
-    
-    result = ask_openai(question, context)
+    answer = ask_openai(question, context)
     return {
       "question": question,
-      "answer": result,
+      "answer": answer,
       "source": "OpenAI",
       "top_k": top_k,
       "chunks": clean_chunks
     }
 
   else:
-    raise ValueError("Provedor inválido. Use 'local' ou 'openai'.")
+      raise ValueError("Provedor inválido. Use 'local' ou 'openai'.")
