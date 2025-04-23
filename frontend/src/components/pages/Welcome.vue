1fb6a1a159768135c12aab513b59fd42f69b0c0d
Kauã Rodrigues <kauarodrigues.fn@gmail.com>
2025-04-23 13:52:09 -0300

    feat(frontend): criar página Welcome

---
+++ frontend/src/components/pages/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center vh-100 px-3">
+    <WelcomeCard />
+  </div>
+</template>
+
+<script setup>
+import WelcomeCard from '../WelcomePage/WelcomeCard.vue'
+</script>
